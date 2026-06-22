61555cb227e04942cfe13d639e27fea7133d6d13
osva1312 <osvaldolazaroh@gmail.com>
2026-06-22 19:25:30 +0200

    terminato esercizio js

---
+++ script.js
@@ -0,0 +1,16 @@
+var num1 = 10;
+var num2 = 20;
+
+console.log(num1);
+console.log(num2);
+
+var stringa = 'hello world';
+
+console.log(stringa);
+var stringa = 'hi planet';
+console.log(stringa);
+
+const PIGRECO = 3.14159265359;
+console.log (PIGRECO);
+const PIGRECO = 3.15;
+
