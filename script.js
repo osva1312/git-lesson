949b30a76fde9bd9fa1f02ade5b94fe3d8ef7679
osva1312 <osvaldolazaroh@gmail.com>
2026-06-25 15:04:10 +0200

    terminato esercizio operatori

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
 let num1 = 10;
 let num2 = 20;
+let num3 = 30;
+let num4 = 40;
+let num5 = 50;
 
-console.log("il valore della variabile num1 è : " + num1);
-console.log("il valore della variabile num2 è : " + num2);
-
-
-let stringa = 'ciao a tutti'
-console.log("il valore della variabile stringa è :" + stringa);
-
-stringa = 'benvenuti'
-console.log("il valore della variabile stringa è :" + stringa);
-
-
-const pGreco = 3.14;
-
-
-
-pGreco = 100;
-
-console.log("il valore di pGreco è :" + pGreco);
+let somma = num1 + num2 + num3 + num4 + num5;
+let media = somma / 5;
+console.log(`La somma tra i numeri equivale a ${somma} e la media equivale a ${media}`);
