925bba37c04ed086c780450207423c62386ca480
osva1312 <osvaldolazaroh@gmail.com>
2026-06-25 15:21:15 +0200

    terminato operatori 2

---
+++ main.js
@@ -0,0 +1,10 @@
+let anno_corrente = 2026;
+let anno_di_nascita = 1999;
+
+let eta = anno_corrente - anno_di_nascita;
+console.log(eta)
+
+let anni = 100 - eta;
+console.log(anni)
+
+console.log("Hai 27 anni e ti mancano 73 anni per compierne 100")
