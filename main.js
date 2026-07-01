cc387109e7111ba86bab4428fdafa88b333f503b
osva1312 <osvaldolazaroh@gmail.com>
2026-07-01 19:09:16 +0200

    terminato esercizio condizioni

--- main.js
+++ main.js
@@ -1,9 +1,44 @@
-let totale_gatti = 40;
-let gatti_in_fila = 5;
- 
-let file_resultanti = Math.floor(totale_gatti / gatti_in_fila);
-let gatti_avanzati = totale_gatti % gatti_in_fila;
+let v = 24;
 
-let gatti_mancanti = gatti_avanzati === 0;
- 
-console.log("Ci sono 8 file di gatti e ne mancano 0 per una nuova fila, con un avanzo di 0");
+if (v < 18){
+    console.log("insuficente");}
+
+    else if ( v >= 18 && v < 21) {
+        console.log(" sufficente");
+    }
+    else if ( v >= 21 && v < 24){
+        console.log("buono")
+    }
+    else if ( v >= 24 && v < 27){
+        console.log ('distinto')
+    }
+    else if ( v >= 27 && v <= 29){
+        console.log ('ottimo')
+    }
+    else if( v = 30){
+        console.log( 'eccellente')
+    }
+        
+    switch(true) {
+        case v < 18 :
+            console.log(" insuficente");
+            break;
+        case v >= 18 && v < 21 :
+            console.log("sufficente")
+            break;
+        case v >= 21 && v < 24 :
+            console.log ("buono");
+            break;
+        case v >= 24 && v < 27 :
+            console.log("distinto");
+            break;
+        case v >= 27 && v <= 29 :
+            console.log("ottimo");
+            break;
+        case v = 30 :
+            console.log( "eccellente")
+            break;
+            default:
+                console.log('Voto non valido')
+    }
+
